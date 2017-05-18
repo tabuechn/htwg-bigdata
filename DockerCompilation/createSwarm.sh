7f821c7cdeed803e951f88a7af273af63f87fd96
daschwin <daschwin@htwg-konstanz.de>
2017-05-18 14:26:03 +0200

    create worker erstellt

--- DockerCompilation/createSwarm.sh
+++ DockerCompilation/createSwarm.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 MANAGERIP="192.168.99.101"
-echo $MANAGERIP
 
 # manager1 löschen
-docker-machine rm -rf manager1
+docker-machine rm -f manager1
 
 # neuen manager erstellen
 docker-machine create --driver virtualbox manager1
@@ -11,6 +10,11 @@ MANAGERIP=$(docker-machine ip manager1)
 echo "########################################################################"
 echo $MANAGERIP
 echo "########################################################################"
+
+# Portforwarding aktivieren
+VBoxManage controlvm "manager1" natpf1 "tcp-port27020,tcp,,27020,,27020"
+VBoxManage controlvm "manager1" natpf1 "tcp-port27021,tcp,,27021,,27021"
+
 
 echo "########################################################################"
 echo "images bauen"
@@ -64,6 +68,7 @@ docker-machine ssh manager1 docker service create -p 27020:27020 --name mainserv
 # Workerserver-Service erstellen
 docker-machine ssh manager1 docker service create -p 27021:27021 --name workerservice akka-http-microservice-workerserver:1.0
 # Actorsystem-Service erstellen
+sleep 2
 docker-machine ssh manager1 docker service create --name actorservice actorsystem:1.0
 
 sh ../TerminalGui/ant_gui.sh $MANAGERIP:27020
---
+++ DockerCompilation/createWorker.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+WORKERIP="192.168.99.101"
+SWARMTOKEN="XYZ123"
+MANAGERIP="141.37.160.134"
+MANAGERPORT="2377"
+
+# worker1 löschen
+docker-machine rm -f worker
+
+# neuen worker erstellen
+docker-machine create --driver virtualbox worker
+WORKERIP=$(docker-machine ip worker)
+echo "########################################################################"
+echo $WORKERIP
+echo "########################################################################"
+
+# Portforwarding aktivieren
+VBoxManage controlvm "worker" natpf1 "tcp-port27020,tcp,,27020,,27020"
+VBoxManage controlvm "worker" natpf1 "tcp-port27021,tcp,,27021,,27021"
+
+
+echo "########################################################################"
+echo "images bauen"
+echo "########################################################################"
+cd ../MainServer
+sbt docker:publishLocal
+cd ../WorkerServer
+sbt docker:publishLocal
+cd ../ActorSystem
+sbt docker:publishLocal
+
+echo "########################################################################"
+echo "Mainserver images speichern und auf worker schieben"
+echo "########################################################################"
+docker save -o main.tar akka-http-microservice-mainserver:1.0
+docker-machine scp main.tar worker:.
+# in Manager einloggen und Images laden
+docker-machine ssh worker docker load < main.tar
+# in Manager einloggen und Images löschen
+docker-machine ssh worker rm -rf main.tar
+
+
+echo "########################################################################"
+echo "Workerserver images speichern und auf worker schieben"
+echo "########################################################################"
+docker save -o worker.tar akka-http-microservice-workerserver:1.0
+docker-machine scp worker.tar worker:.
+# in Manager einloggen Images laden
+docker-machine ssh worker docker load<worker.tar
+# in Manager einloggen und Images löschen
+docker-machine ssh worker rm -rf worker.tar
+
+echo "########################################################################"
+echo "Actorsystem images speichern und auf worker schieben"
+echo "########################################################################"
+docker save -o actor.tar actorsystem:1.0
+docker-machine scp actor.tar worker:.
+# in Manager einloggen Images laden
+docker-machine ssh worker docker load<actor.tar
+# in Manager einloggen und Images löschen
+docker-machine ssh worker rm -rf actor.tar
+
+docker swarm join --token $SWARMTOKEN $MANAGERIP:$MANAGERPORT
