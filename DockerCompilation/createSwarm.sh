c2276e5c9ec3915c9000a62796b44a9d0afcb4fa
daschwin <daschwin@htwg-konstanz.de>
2017-05-18 13:27:00 +0200

    swarm creation erweitert

--- DockerCompilation/createSwarm.sh
+++ DockerCompilation/createSwarm.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
+MANAGERIP="192.168.99.101"
+echo $MANAGERIP
 
 # manager1 löschen
-docker-machine rm manager1
+docker-machine rm -rf manager1
 
 # neuen manager erstellen
 docker-machine create --driver virtualbox manager1
+MANAGERIP=$(docker-machine ip manager1)
+echo "########################################################################"
+echo $MANAGERIP
+echo "########################################################################"
 
-# images bauen
+echo "########################################################################"
+echo "images bauen"
+echo "########################################################################"
 cd ../MainServer
 sbt docker:publishLocal
 cd ../WorkerServer
@@ -14,28 +22,48 @@ sbt docker:publishLocal
 cd ../ActorSystem
 sbt docker:publishLocal
 
-# images speichern und auf manager schieben
+echo "########################################################################"
+echo "Mainserver images speichern und auf manager schieben"
+echo "########################################################################"
 docker save -o main.tar akka-http-microservice-mainserver:1.0
 docker-machine scp main.tar manager1:.
+# in Manager einloggen und Images laden
+docker-machine ssh manager1 docker load < main.tar
+# in Manager einloggen und Images löschen
+docker-machine ssh manager1 rm -rf main.tar
+
+
+echo "########################################################################"
+echo "Workerserver images speichern und auf manager schieben"
+echo "########################################################################"
 docker save -o worker.tar akka-http-microservice-workerserver:1.0
 docker-machine scp worker.tar manager1:.
+# in Manager einloggen Images laden
+docker-machine ssh manager1 docker load<worker.tar
+# in Manager einloggen und Images löschen
+docker-machine ssh manager1 rm -rf worker.tar
+
+echo "########################################################################"
+echo "Actorsystem images speichern und auf manager schieben"
+echo "########################################################################"
 docker save -o actor.tar actorsystem:1.0
 docker-machine scp actor.tar manager1:.
+# in Manager einloggen Images laden
+docker-machine ssh manager1 docker load<actor.tar
+# in Manager einloggen und Images löschen
+docker-machine ssh manager1 rm -rf actor.tar
 
-# in Manager einloggen
-docker-machine ssh manager1
+echo "########################################################################"
+echo "Swarm erstellen"
+echo "########################################################################"
+docker-machine ssh manager1 docker swarm init --advertise-addr $MANAGERIP
+# Registry TODO ?!
 
-# Images laden
-docker load < main.tar
-docker load < worker.tar
-docker load < actor.tar
+# Mainserver-Service erstellen
+docker-machine ssh manager1 docker service create -p 27020:27020 --name mainservice akka-http-microservice-mainserver:1.0
+# Workerserver-Service erstellen
+docker-machine ssh manager1 docker service create -p 27021:27021 --name workerservice akka-http-microservice-workerserver:1.0
+# Actorsystem-Service erstellen
+docker-machine ssh manager1 docker service create --name actorservice actorsystem:1.0
 
-# Swarm erstellen
-docker swarm init --advertise-addr 192.168.99.101
-# Registry TODO
-
-docker service create -p 27020:27020 --name mainservice akka-http-microservice-mainserver:1.0
-
-docker save -o worker.tar akka-http-microservice-workerserver:1.0
-
-docker-machine ssh manager1
+sh ../TerminalGui/ant_gui.sh $MANAGERIP:27020
